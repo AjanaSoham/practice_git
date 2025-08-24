6f3109a029fc8871957713c159b34b322a0fec01
Soham Jana <sohamjana396@gmail.com>
2025-08-24 23:03:55 +0530

    Will try to resolve array

--- Java/FullBank.java
+++ Java/FullBank.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Account {
     double bal;
@@ -47,7 +48,28 @@ class Current extends Account {
 
 public class FullBank {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("MENU \n 1. Create Savings Account \n 2. Create Current Account \n 3. Exit");
+
+        System.out.print("Enter Choice: ");
+        int ch1 = sc.nextInt();
+
+        switch (ch1){
+            case 1:
+                System.out.println("Enter Account number: ");
+                String acno = sc.next();
+                System.out.println("Enter opening balance: ");
+                Double val = sc.nextDouble();
+                System.out.println("Enter Interest amount: ");
+                Double interest = sc.nextDouble();
+                Savings.add(new Savings(acno, val, interest));
+                
+        }
+        System.out.println("MENU: \n 1. Show balance\n 2. Deposit\n 3. Withdraw\n 4. Exit");
+
+        
         
     }
 
